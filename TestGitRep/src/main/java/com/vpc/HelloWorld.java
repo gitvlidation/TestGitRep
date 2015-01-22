378a3d62387dc9d7abc366b741bc060c7d9b6724
gitvlidation <gitvlidation@gmail.com>
2015-01-23 01:23:41 +0530

    Initial checkin

---
+++ TestGitRep/src/main/java/com/vpc/HelloWorld.java
@@ -0,0 +1,15 @@
+package main.java.com.vpc;
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello..........");
+
+	}
+
+}
